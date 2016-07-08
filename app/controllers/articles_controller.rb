e0c8f4d379e93e713b81bbbc9adb126d4cccc6f9
davidhostios <davidhostios@gmail.com>
2016-07-08 14:03:48 -0500

    finish initial crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,14 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end	
+
 	def create
-		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
 
 		if @article.save
@@ -20,6 +24,23 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def update
+		@article = Article.new(article_params)
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private 
